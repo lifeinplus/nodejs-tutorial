d5db04871e011cc01707d9f37b4c9f6338494450
Artem Denisov <lifeinplus@gmail.com>
2024-06-11 00:56:25 +0200

    NPM Node Package Manager Modules | NPM Tutorial for Beginners

--- index.js
+++ index.js
@@ -1,62 +1,6 @@
-const fsPromises = require("fs").promises;
-const path = require("path");
+const { format } = require("date-fns");
+const { v4: uuid } = require("uuid");
 
-const fileOps = async () => {
-    try {
-        const data = await fsPromises.readFile(
-            path.join(__dirname, "files", "starter.txt"),
-            "utf8"
-        );
+console.log(format(new Date(), "yyyyMMdd\tHH:mm:ss"));
 
-        console.log(data);
-
-        await fsPromises.unlink(path.join(__dirname, "files", "starter.txt"));
-
-        await fsPromises.writeFile(
-            path.join(__dirname, "files", "promiseWrite.txt"),
-            data
-        );
-
-        await fsPromises.appendFile(
-            path.join(__dirname, "files", "promiseWrite.txt"),
-            "\n\nTesting text."
-        );
-
-        await fsPromises.rename(
-            path.join(__dirname, "files", "promiseWrite.txt"),
-            path.join(__dirname, "files", "promiseComplete.txt")
-        );
-
-        const newData = await fsPromises.readFile(
-            path.join(__dirname, "files", "promiseComplete.txt"),
-            "utf8"
-        );
-
-        console.log(newData);
-    } catch (error) {
-        console.error();
-    }
-};
-
-fileOps();
-
-//
-//             (error) => {
-//                 if (error) throw error;
-//                 console.log("Append complete");
-
-//
-//                     (error) => {
-//                         if (error) throw error;
-//                         console.log("Rename complete");
-//                     }
-//                 );
-//             }
-//         );
-//     }
-// );
-
-process.on("uncaughtException", (error) => {
-    console.log("There was an uncaught error:", error);
-    process.exit(1);
-});
+console.log(uuid());

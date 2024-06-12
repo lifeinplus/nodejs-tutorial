7d141f4c8e1eb4802ef5a832c1277fd888159510
Artem Denisov <lifeinplus@gmail.com>
2024-06-13 01:27:46 +0200

    DEV-79 Node.js Tutorials for Beginners - How to Setup Routes with Express Router

--- server.js
+++ server.js
@@ -32,20 +32,12 @@ app.use(cors(corsOptions));
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-app.use(express.static(path.join(__dirname, "/public")));
+app.use("/", express.static(path.join(__dirname, "/public")));
+app.use("/subdir", express.static(path.join(__dirname, "/public")));
 
-app.get("^/$|/index(.html)?", (req, res) => {
-    // res.sendFile("./views/index.html", { root: __dirname });
-    res.sendFile(path.join(__dirname, "views", "index.html"));
-});
-
-app.get("/new-page(.html)?", (req, res) => {
-    res.sendFile(path.join(__dirname, "views", "new-page.html"));
-});
-
-app.get("/old-page(.html)?", (req, res) => {
-    res.redirect(301, "new-page.html");
-});
+app.use("/", require("./routes/root"));
+app.use("/subdir", require("./routes/subdir"));
+app.use("/employees", require("./routes/api/employees"));
 
 app.get(
     "/hello(.html)?",
@@ -57,23 +49,6 @@ app.get(
         res.send("Hello World!");
     }
 );
-
-const one = (req, res, next) => {
-    console.log("one");
-    next();
-};
-
-const two = (req, res, next) => {
-    console.log("two");
-    next();
-};
-
-const three = (req, res, next) => {
-    console.log("three");
-    res.send("Finished!");
-};
-
-app.get("/chain(.html)?", [one, two, three]);
 
 app.all("*", (req, res) => {
     res.status(404);

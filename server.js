5bf8953ec92adf2ee1da62efbd09421a6eccff1a
Artem Denisov <lifeinplus@gmail.com>
2024-06-12 02:57:39 +0200

    DEV-79 Node.js Tutorials for Beginners - What is Middleware in Express JS?

--- server.js
+++ server.js
@@ -1,7 +1,38 @@
+const cors = require("cors");
 const express = require("express");
-const app = express();
 const path = require("path");
+
+const errorHandler = require("./middleware/errorHandler");
+const { logger } = require("./middleware/logEvents");
+
+const app = express();
 const PORT = process.env.PORT || 3500;
+
+app.use(logger);
+
+const whitelist = [
+    "https://www.yoursite.com",
+    "http://127.0.0.1:5500",
+    "http://localhost:3500",
+];
+
+const corsOptions = {
+    origin: (origin, callback) => {
+        if (whitelist.indexOf(origin) !== -1 || !origin) {
+            callback(null, true);
+        } else {
+            const error = new Error("Not allowed by CORS");
+            callback(error);
+        }
+    },
+    optionsSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "/public")));
 
 app.get("^/$|/index(.html)?", (req, res) => {
     // res.sendFile("./views/index.html", { root: __dirname });
@@ -44,9 +75,18 @@ const three = (req, res, next) => {
 
 app.get("/chain(.html)?", [one, two, three]);
 
-app.get("/*", (req, res) => {
-    res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
+app.all("*", (req, res) => {
+    res.status(404);
+    if (req.accepts("html")) {
+        res.sendFile(path.join(__dirname, "views", "404.html"));
+    } else if (req.accepts("json")) {
+        res.json({ error: "404 Not Found" });
+    } else {
+        res.type("text").send("404 Not Found");
+    }
 });
+
+app.use(errorHandler);
 
 app.listen(PORT, () => {
     console.log("Server running on port " + PORT);

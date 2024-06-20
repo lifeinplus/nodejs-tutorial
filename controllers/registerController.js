9c9e825ef9d185a34556d2444f602b1bfb775aac
Artem Denisov <lifeinplus@gmail.com>
2024-06-21 00:59:35 +0200

    DEV-79 Node.js Tutorials for Beginners - How to create MongoDB Schemas and Data Models

--- controllers/registerController.js
+++ controllers/registerController.js
@@ -1,13 +1,5 @@
-const fs = require("fs");
 const bcrypt = require("bcrypt");
-const path = require("path");
-
-const usersDB = {
-    users: require("../model/users.json"),
-    setUsers: function (data) {
-        this.users = data;
-    },
-};
+const User = require("../model/User");
 
 const handleNewUser = async (req, res) => {
     const { username, password } = req.body;
@@ -18,25 +10,20 @@ const handleNewUser = async (req, res) => {
         });
     }
 
-    const duplicate = usersDB.users.find((user) => user.username === username);
+    const duplicate = await User.findOne({ username }).exec();
 
     if (duplicate) return res.sendStatus(409);
 
     try {
         const hashedPassword = await bcrypt.hash(password, 10);
-        const newUser = {
+
+        const result = await User.create({
             username,
-            roles: { user: 2001 },
             password: hashedPassword,
-        };
-        usersDB.setUsers([...usersDB.users, newUser]);
+        });
 
-        await fs.promises.writeFile(
-            path.join(__dirname, "..", "model", "users.json"),
-            JSON.stringify(usersDB.users)
-        );
+        console.log(result);
 
-        console.log(usersDB.users);
         return res
             .status(201)
             .json({ success: `New user ${username} created!` });
